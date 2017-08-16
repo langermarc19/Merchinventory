e0c7417ab2250668d5f64ae5ff8f7b1c91a7db7c
Marc Langer <langer.marc.19@gmail.com>
2017-08-16 11:37:47 -0500

    No longer need to refresh after adding new item

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@
 class PagesController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
-	before_action :set_items_list, only: [:post_update_stock, :update_stock, :inventory]
+	before_action :set_items_list, only: [:post_add_item, :post_update_stock, :update_stock, :inventory]
 	before_action :set_item, only: [:update_stock, :post_update_stock]
 
 
@@ -13,6 +13,7 @@ class PagesController < ApplicationController
 		@price = params[:price]
 
 		Item.add_item(@item_name, @size, @price) 
+		render :inventory
 	end
 
 	def post_update_stock
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'pages#inventory'
 
   get '/inventory' , to: 'pages#inventory'
   post '/inventory' , to: 'pages#post_add_item'
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post '/items/:id/update_stock' , to: 'pages#post_update_stock'
 
 
-  
+
 
 
 
